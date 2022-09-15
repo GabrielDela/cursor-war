c685b010f1b2125434d15f9e52346d56a1060ae4
Gabriel <gabrieldelahaye76680@gmail.com>
2022-09-16 01:13:10 +0200

    USERNAME UNIQUE + DATA FORMAT TRANSMITION

--- index.js
+++ index.js
@@ -5,26 +5,42 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var usernames = [];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    // console.log('a user connected');
-
-    console.log("Connection", socket.id);
-
     socket.on('disconnect', () => {
-        console.log("Disconnect", socket.id);
         socket.broadcast.emit('remove', socket.id);
+        
+         // remove the username from global usernames list if connected
+        if(socket.username){
+            usernames.splice(usernames.indexOf(socket.username), 1);
+        }
     });
 
-    socket.on('move', (data) => {
-        data.socket_id = socket.id;
+    socket.on('login', (data) => {
+        if (usernames.includes(data.username)) {
+            socket.emit('login', {success: false, message: 'Username already taken'});
+        } else {
+            usernames.push(data.username);
+            socket.username = data.username;
+            socket.color = data.color;
+            socket.emit('login', {success: true, message: 'Logged in'});
+        }
+    });
+
+    socket.on('move', (position) => {
+        let data = {
+            ...position,
+            username: socket.username,
+            color: socket.color,
+            socket_id: socket.id,
+        };
         socket.broadcast.emit('move', data);
     });
-
 });
 
 server.listen(3000, () => {
